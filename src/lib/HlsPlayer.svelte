49af9a08665a8ac5d407cbb8533aebf371d7010f
三木 <578595193@qq.com>
2022-08-02 11:42:34 +0800

    FEAT: init project

---
+++ src/lib/HlsPlayer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<!--Add this line to your web component-->
+<svelte:options tag="hls-player" />
+
+<button on:click={increment}>
+  count is {count}
+</button>
